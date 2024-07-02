2bb4e57cb1b3faad765a61453361f927894e83b4
vasmaae <gutorov.ia1@gmail.com>
2024-07-02 10:31:16 +0400

    final prac 5 var 10

--- IntroductoryEducationalPractice/IntroductoryEducationalPractice/FormLogging.cs
+++ IntroductoryEducationalPractice/IntroductoryEducationalPractice/FormLogging.cs
@@ -10,13 +10,27 @@ public partial class FormLogging : Form
     {
         InitializeComponent();
         _logger = logger;
+        _logger.LogInformation("Программа запущена");
     }
 
     private void ButtonCalculate_Click(object sender, EventArgs e)
     {
         try
         {
+            double varX = double.Parse(textBoxXValue.Text), funcY, sumS = 0;
 
+            _logger.LogInformation("Начато вычисление функции Y");
+            funcY = Math.Log(1 - Math.Pow(varX, 2));
+            _logger.LogInformation($"Значение функции Y = {funcY.ToString()}");
+            textBoxFuncY.Text = funcY.ToString();
+
+            _logger.LogInformation("Начато вычисление суммы S");
+            for (int i = 1; i <= 20; i++)
+            {
+                sumS += -1 * (Math.Pow(varX, i * 2) / i);
+            }
+            _logger.LogInformation($"Значение суммы S = {sumS.ToString()}");
+            textBoxSumS.Text = sumS.ToString();
         }
         catch (Exception ex)
         {
